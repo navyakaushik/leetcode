44730dfc8a48d7ce09db2ec632ecbac53aa3e373
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-06-14 21:10:02 +0530

    Time: 37 ms (72.13%), Space: 60.6 MB (18.03%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+
+        Arrays.sort(nums);
+        int res = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] >= nums[i]) {
+                res += 1 + nums[i - 1] - nums[i];
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+
+        return res;
+        
+    }
+}
