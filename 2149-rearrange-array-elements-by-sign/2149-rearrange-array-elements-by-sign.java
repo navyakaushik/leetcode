f0c5fbb322e1cb1085b36504c58cce736b883ad9
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-09-03 17:36:00 +0530

    Time: 9 ms (21.04%), Space: 82.8 MB (18.77%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,22 +1,30 @@
-class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        //brute force
+class Solution{
+    public int[] rearrangeArray(int[] nums){
         int n = nums.length;
+        
+        //Define lists for storing positive and negative elements seperately
         ArrayList<Integer> pos = new ArrayList<>();
         ArrayList<Integer> neg = new ArrayList<>();
         
-        //to segregate the array into positive and negative
+        //segregate the input array into positive and negative numbers
         for(int i = 0; i < n; i++){
-            if(nums[i] > 0) pos.add(nums[i]);
-            else neg.add(nums[i]);
+            if(nums[i] > 0){
+                pos.add(nums[i]);
+            }else{
+                neg.add(nums[i]);
+            }
         }
+        //result array
         
-        //positives on even indeces and negative on odd indeces
-        for(int i = 0; i < n/2; i ++){
-            nums[2*i] = pos.get(i);
-            nums[2*i+1] = neg.get(i);
+        int[] ans = new int[n];
+        int posIndex = 0, negIndex = 1;
+        
+        for(int i = 0; i < pos.size(); i++){
+            ans[posIndex] = pos.get(i);
+            ans[negIndex] = neg.get(i);
+            posIndex += 2;
+            negIndex += 2;
         }
-        return nums;
-        
+        return ans;
     }
 }
