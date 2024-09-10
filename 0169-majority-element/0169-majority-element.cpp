4c1608536218c4e57626d0a6db31f33e4c285f7d
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-09-10 14:27:08 +0530

    Time: 18 ms (52.62%), Space: 27 MB (49.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+    // MOORE'S VOTING ALGORITHM
     int majorityElement(vector<int>& nums) {
-        map<int, int>mpp;
-        for(int i = 0; i < nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it: mpp){
-            if(it.second > nums.size() / 2){
-                return it.first;
+        int cnt = 0;
+        int el = -1;
+        
+        // First loop to find a potential candidate
+        for (int i = 0; i < nums.size(); i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (nums[i] == el) {
+                cnt++;
+            } else {
+                cnt--;
             }
         }
+
+        // Second loop to confirm if the candidate is indeed the majority
+        int cnt1 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == el) cnt1++;
+        }
+
+        // Check if the candidate appears more than n/2 times
+        if (cnt1 > nums.size() / 2) {
+            return el;
+        }
+
         return -1;
-        
     }
 };
