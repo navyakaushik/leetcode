21188bbb31fef38d9ae86d9e92f45de210edbb5a
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-09-11 13:07:42 +0530

    Time: 10 ms (48.35%), Space: 18.7 MB (53.05%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    //better solution
+    // Optimal solution using Moore's Voting Algorithm
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> result;
-        map<int, int> mpp;
-        int n = nums.size();
-        int mini = n/3;
-        
-        for(int i = 0; i < n; i++){
-            mpp[nums[i]]++;
-        }
-        
-        for(auto it : mpp){
-            if(it.second > mini){
-                result.push_back(it.first);
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+
+        // Step 1: Find potential candidates for majority elements
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt1 == 0 && el2 != nums[i]){
+                cnt1 = 1;
+                el1 = nums[i];
+            }
+            else if(cnt2 == 0 && el1 != nums[i]){
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1) cnt1++;
+            else if(nums[i] == el2) cnt2++;
+            else{
+                cnt1--, cnt2--;
             }
         }
-        
-        sort(result.begin(), result.end());
-        return result;
-        
+
+        // Step 2: Validate if the candidates appear more than n/3 times
+        vector<int> ls;
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(el1 == nums[i]) cnt1++;
+            if(el2 == nums[i]) cnt2++;
+        }
+
+        int mini = nums.size() / 3;
+        if(cnt1 > mini) ls.push_back(el1);
+        if(cnt2 > mini) ls.push_back(el2);
+
+        return ls;
     }
-    
 };
