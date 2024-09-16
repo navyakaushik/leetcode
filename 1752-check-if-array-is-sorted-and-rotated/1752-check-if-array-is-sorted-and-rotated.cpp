3e4f825d523cbba1df87dbee50e7210d434a9a4e
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-09-16 22:53:30 +0530

    Time: 4 ms (33.05%), Space: 10.5 MB (62.11%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,11 +4,13 @@ public:
         const int n = nums.size();
         int count = 0;
         
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++){
             if(nums[i] > nums[(i+1) % n]){
                 count++;
             }
         }
-        return count <=1;
+        return count <= 1;
+        
+        
     }
 };
