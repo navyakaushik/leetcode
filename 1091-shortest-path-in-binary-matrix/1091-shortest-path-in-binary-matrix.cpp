9350a785481043a544781f777e55eb755d864bc5
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-10-26 02:49:16 +0530

    Time: 272 ms (6.26%), Space: 82.8 MB (6.60%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> directions{{1, 1}, {0, 1}, {1, 0}, {-1, 0}, {0, -1}, {-1, -1}, {1, -1}, {-1, 1}};
+    typedef pair<int, pair<int, int>> P;
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        if(m == 0 || n == 0 || grid[0][0] != 0)
+        // Check for empty grid or blocked starting cell
+        if (m == 0 || n == 0 || grid[0][0] != 0)
             return -1;
 
-        auto isSafe = [&](int x, int y){
+        auto isSafe = [&](int x, int y) {
             return x >= 0 && x < m && y >= 0 && y < n;
         };
 
-        queue<pair<int, int>> que;
-        que.push({0, 0});
-        grid[0][0] = 1;
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+        priority_queue<P, vector<P>, greater<P>> pq;
 
-        int level = 1;  // Start level from 1 as the first cell is counted
+        pq.push({0, {0, 0}});
+        result[0][0] = 1; // Start from level 1
 
-        while(!que.empty()){
-            int N = que.size();
-            while(N--){
-                auto curr = que.front();
-                que.pop();
+        while (!pq.empty()) {
+            int d = pq.top().first;
+            pair<int, int> node = pq.top().second;
+            pq.pop();
 
-                int x = curr.first;
-                int y = curr.second;
+            int x = node.first;
+            int y = node.second;
 
-                if(x == m - 1 && y == n - 1) return level;  // Reached the bottom-right corner
+            if (x == m - 1 && y == n - 1) // If reached the end
+                return d + 1;
 
-                for(auto& dir : directions){
-                    int x_ = x + dir[0];
-                    int y_ = y + dir[1];
+            for (auto dir : directions) {
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+                int dist = 1;
 
-                    if(isSafe(x_, y_) && grid[x_][y_] == 0){
-                        que.push({x_, y_});
-                        grid[x_][y_] = 1;  // Mark as visited
-                    }
+                if (isSafe(x_, y_) && grid[x_][y_] == 0 && d + dist < result[x_][y_]) {
+                    pq.push({d + dist, {x_, y_}});
+                    result[x_][y_] = d + dist;
                 }
             }
-            level++;
         }
 
+        // If destination is unreachable
         return -1;
     }
 };
