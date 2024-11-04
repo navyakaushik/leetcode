f400d717d34f330c22e44e00fc771c012074f4df
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-05 01:36:30 +0530

    Time: 397 ms (20.52%), Space: 113.7 MB (16.87%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -3,23 +3,28 @@ public:
     int N;
     long result_base_node;
     vector<int> count;
-
+    
     int dfsBase(unordered_map<int, vector<int>> &adj, int curr_node, int prev_node, int curr_depth) {
         int total_node = 1;
-        result_base_node += curr_depth;  // Accumulate distances to calculate result_base_node
+        result_base_node += curr_depth;
+
         for (int child : adj[curr_node]) {
-            if (child == prev_node) continue;  // Avoid revisiting the parent node
+            if (child == prev_node) continue;
+
             total_node += dfsBase(adj, child, curr_node, curr_depth + 1);
         }
-        count[curr_node] = total_node;  // Update the count of nodes in subtree rooted at curr_node
+
+        count[curr_node] = total_node;
         return total_node;
     }
 
     void DFS(unordered_map<int, vector<int>> &adj, int parent_node, int prev_node, vector<int>& result) {
         for (int &child : adj[parent_node]) {
-            if (child == prev_node) continue;  // Avoid revisiting the parent node
+            if (child == prev_node) continue;
+
             result[child] = result[parent_node] - count[child] + (N - count[child]);
-            DFS(adj, child, parent_node, result);  // Recursive DFS for other nodes
+
+            DFS(adj, child, parent_node, result);
         }
     }
 
@@ -28,7 +33,6 @@ public:
         N = n;
         count.resize(n, 0);
 
-        // Build adjacency list
         for (auto &vec : edges) {
             int u = vec[0];
             int v = vec[1];
@@ -36,13 +40,13 @@ public:
             adj[v].push_back(u);
         }
 
-        result_base_node = 0;  // Initialize result for base node
-        dfsBase(adj, 0, -1, 0);  // Calculate distances for base node
+        result_base_node = 0;
+        dfsBase(adj, 0, -1, 0);
 
         vector<int> result(n, 0);
-        result[0] = result_base_node;  // Set base result for root node
+        result[0] = result_base_node;
 
-        DFS(adj, 0, -1, result);  // Calculate results for all other nodes
+        DFS(adj, 0, -1, result);
 
         return result;
     }
