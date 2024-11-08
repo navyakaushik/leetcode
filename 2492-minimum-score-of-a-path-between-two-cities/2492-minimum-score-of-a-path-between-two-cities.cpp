a4374be4cab29098f1b2d3ba0dbf44ba2316da9a
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-08 23:24:18 +0530

    Time: 109 ms (42.99%), Space: 140.7 MB (44.91%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
+    
+    void dfs( int u , unordered_map<int, vector<pair<int, int>>>& adj, vector<bool>& visited, int & result){
+        
+        visited[u] = true;
+        
+        for(auto &p : adj[u]){
+            int v = p.first;
+            int d = p.second;
+            
+            result = min(result, d);
+            
+            if(!visited[v]){
+                dfs(v ,adj, visited, result);
+            }
+        }
+        
+    }
     int minScore(int n, vector<vector<int>>& roads) {
+        
         unordered_map<int, vector<pair<int, int>>> adj;
-
-        // Construct the adjacency list
-        for (auto &vec : roads) {
+        
+        for(vector<int>& vec : roads){
             int u = vec[0];
             int v = vec[1];
             int d = vec[2];
-
+            
             adj[u].push_back({v, d});
-            adj[v].push_back({u, d});
+            adj[v].push_back({u , d});
         }
-
-        vector<bool> visited(n + 1, false);  // 1-based indexing
+        
+        vector<bool> visited(n , false);
         int result = INT_MAX;
-
-        // Start DFS from node 1 (or an arbitrary starting point)
         dfs(1, adj, visited, result);
-
         return result;
-    }
-
-
-    void dfs(int u, unordered_map<int, vector<pair<int, int>>>& adj, vector<bool>& visited, int& result) {
-        visited[u] = true;
-
-        for (auto &p : adj[u]) {
-            int v = p.first;
-            int d = p.second;
-
-            // Update the result with the minimum edge weight in the connected component
-            result = min(result, d);
-
-            if (!visited[v]) {
-                dfs(v, adj, visited, result);
-            }
-        }
     }
 };
