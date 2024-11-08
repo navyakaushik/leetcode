466f74411d72995813eb3198d058d415bc1cf11c
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-09 00:32:13 +0530

    Time: 326 ms (26.19%), Space: 127.2 MB (37.45%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:         
+    int count = 0;
+
+    void DFS(int u, int parent, unordered_map<int, vector<pair<int, int>>>& adj) {
+        for (auto &p : adj[u]) {
+            int v = p.first;
+            int check = p.second;
+            if (v != parent) {
+                if (check == 1) {
+                    count++;
+                }
+                DFS(v, u, adj);
+            }
+        }
+    }
+
+    int minReorder(int n, vector<vector<int>>& connections) {              
+        unordered_map<int, vector<pair<int, int>>> adj;              
+        for (auto &vec : connections) {
+            int a = vec[0];
+            int b = vec[1];
+            adj[a].push_back({b, 1});
+            adj[b].push_back({a, 0});
+        }
+        DFS(0, -1, adj);
+        return count;                      
+    }
+};
