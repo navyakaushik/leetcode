73728c0845b82352a8cf95e46c89f5c948a61cfb
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-08 23:08:41 +0530

    Time: 5 ms (37.89%), Space: 17.2 MB (68.24%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // Initialize distances to max and adjacency list
+        
+        // initialize the distances to max and the adjacency list
         vector<int> distance(n, INT_MAX);
         unordered_map<int, vector<pair<int, int>>> adj;
-
-        // Construct adjacency list for the graph
-        for (vector<int>& vec : flights) {
+        
+        //unordered list for the graph
+        for(vector<int>& vec : flights){
             int u = vec[0];
             int v = vec[1];
             int cost = vec[2];
-            adj[u].push_back({v, cost});
+            adj[u].push_back({v ,cost});
         }
-
-        // Initialize queue and set the distance to the source as 0
+        
+        //initialize the queue and set the source as 0
         queue<pair<int, int>> que;
         que.push({src, 0});
         distance[src] = 0;
-
+        
         int steps = 0;
-
-        // Perform BFS until 'k' stops
-        while (!que.empty() && steps <= k) {
+        
+        //apply bfs until we get k stops
+        
+        while(!que.empty() && steps <= k){
             int N = que.size();
-            vector<int> tempDist = distance; // Use temp array for this level's updates
-            while (N--) {
+            vector<int> tempDist = distance;
+            while(N--){
                 int u = que.front().first;
                 int d = que.front().second;
                 que.pop();
-
-                for (pair<int, int>& p : adj[u]) {
+                
+                for(pair<int, int>& p : adj[u]){
                     int v = p.first;
                     int cost = p.second;
-
-                    // Relaxation condition
-                    if (tempDist[v] > d + cost) {
+                    
+                    if(tempDist[v] > d + cost){
                         tempDist[v] = d + cost;
-                        que.push({v, d + cost});
+                        que.push({v , d + cost});
                     }
                 }
             }
-            distance = tempDist; // Update distances for next level
+            distance = tempDist;
             steps++;
+            
         }
-
         return (distance[dst] == INT_MAX) ? -1 : distance[dst];
+        
+        
+        
     }
 };
