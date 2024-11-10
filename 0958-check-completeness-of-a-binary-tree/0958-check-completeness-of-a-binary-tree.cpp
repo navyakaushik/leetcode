2e214a2824f35142de9c48bf85566c74d8622eb1
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-10 17:34:13 +0530

    Time: 0 ms (100.00%), Space: 13 MB (70.52%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -1,16 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isCompleteTree(TreeNode* root) {
-        if (root == NULL)
-            return true;
-        
-        int totalNodes = countNodes(root); 
-        return dfs(root, 1, totalNodes); 
-    }
-    
     int countNodes(TreeNode* root) {
-        if (root == NULL)
+        if (root == NULL) {
             return 0;
+        }
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
 
@@ -23,5 +27,11 @@ public:
         
         return dfs(root->left, 2 * i, totalNodes) &&
                dfs(root->right, 2 * i + 1, totalNodes);
+    }
+
+    bool isCompleteTree(TreeNode* root) {
+        int totalNodes = countNodes(root);
+        int i = 1;
+        return dfs(root, i, totalNodes);
     }
 };
