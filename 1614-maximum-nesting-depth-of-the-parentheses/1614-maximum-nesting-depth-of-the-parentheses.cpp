c1c97c33abae6172defdfaf5a63bd46bc6255a00
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-15 18:09:23 +0530

    Time: 2 ms (5.12%), Space: 7.9 MB (31.09%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int maxDepth(string s) {
         
-        stack<char> st;
-        
-        int result = 0;
+        int openBrackets = 0;
+        int result = -0;
         
         for(char &ch : s){
             if(ch == '('){
-                st.push(ch);
-            }else if(ch == ')'){
-                st.pop();
+                openBrackets++;
+            } else if(ch == ')'){
+                openBrackets--;
             }
-            
-            result = max(result,(int)st.size());
+            result = max(result, openBrackets);
         }
-        
         return result;
-        
     }
 };
