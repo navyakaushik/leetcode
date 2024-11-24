8b319b7c5f8d3e16f6cbf228848b48f840e72d6e
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-24 18:32:21 +0530

    Time: 48 ms (22.86%), Space: 11.6 MB (50.97%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    
-    int t[1001][1001];
-    bool check(string &s, int i, int j){
-        if(i > j){
-            return true;
-        }
-        
-        if (t[i][j] != -1){
-            return t[i][j];
-        }
-        
-        
-        if(s[i] == s[j]){
-            return t[i][j] = check(s, i+1, j-1);
-        }
-        return t[i][j] = false;
-    }
-    
-    int countSubstrings(string s) {
+    int countSubstrings(string s) { // Updated function name to 'countSubstrings'
         int n = s.length();
-        memset(t, -1, sizeof(t));
-        
- 
+        vector<vector<bool>> t(n, vector<bool>(n, false));
         int count = 0;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                
-                if(check(s,i,j)){
+
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L - 1 < n; i++) {
+                int j = i + L - 1;
+
+                if (i == j) {
+                    t[i][i] = true;
+                } else if (i + 1 == j) {
+                    t[i][j] = (s[i] == s[j]);
+                } else {
+                    t[i][j] = (s[i] == s[j] && t[i + 1][j - 1]);
+                }
+
+                if (t[i][j] == true) {
                     count++;
                 }
             }
