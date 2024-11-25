36af2286e078c6e923752ff0007eadaaf8e96d8a
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-11-25 15:33:08 +0530

    Time: 47 ms (23.62%), Space: 11.5 MB (57.87%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int countSubstrings(string s) { // Updated function name to 'countSubstrings'
+    int countSubstrings(string s) {
+        
         int n = s.length();
+        
         vector<vector<bool>> t(n, vector<bool>(n, false));
+        
+        
         int count = 0;
-
-        for (int L = 1; L <= n; L++) {
-            for (int i = 0; i + L - 1 < n; i++) {
+        
+        for(int L = 1; L <= n; L++){
+            for(int i = 0; i + L - 1 < n; i++){
+                
                 int j = i + L - 1;
-
-                if (i == j) {
-                    t[i][i] = true;
-                } else if (i + 1 == j) {
+                
+                if(i == j){
+                    t[i][j] = true;
+                } else if(i + 1 == j){
                     t[i][j] = (s[i] == s[j]);
                 } else {
-                    t[i][j] = (s[i] == s[j] && t[i + 1][j - 1]);
+                    
+                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]);
                 }
-
-                if (t[i][j] == true) {
+                if(t[i][j] == true){
                     count++;
                 }
             }
+
         }
         return count;
+        
     }
 };
