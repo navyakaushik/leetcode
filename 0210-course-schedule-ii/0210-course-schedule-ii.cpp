7a7a80fdb26c1dcebc859d07b1795f1d9438e594
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-01 15:36:52 +0530

    Time: 4 ms (62.98%), Space: 18.5 MB (26.01%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    vector<int> topologicalSort(unordered_map<int, vector<int>> &adj, int n, vector<int>& indegree){
+    vector<int> topoSort(unordered_map<int, vector<int>>&adj, int n, vector<int>& indegree){
         
         queue<int> que;
         vector<int> result;
@@ -9,12 +9,14 @@ public:
         
         for(int i = 0; i < n; i++){
             if(indegree[i] == 0){
-                result.push_back(i);
                 count++;
+                result.push_back(i);
                 que.push(i);
             }
         }
+        
         while(!que.empty()){
+            
             int u = que.front();
             que.pop();
             
@@ -22,20 +24,20 @@ public:
                 indegree[v]--;
                 
                 if(indegree[v] == 0){
-                    result.push_back(v);
                     count++;
                     que.push(v);
+                    result.push_back(v);
                 }
             }
         }
-        if(count == n)
-            return result;
+        
+        if(count == n) return result;
         return {};
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         
         unordered_map<int, vector<int>> adj;
-        vector<int> indegree(numCourses, 0); //kahns algorithm
+        vector<int> indegree(numCourses, 0);
         
         for(auto &vec : prerequisites){
             int a = vec[0];
@@ -44,7 +46,7 @@ public:
             adj[b].push_back(a);
             indegree[a]++;
         }
-        return topologicalSort(adj, numCourses, indegree);
+        return topoSort(adj, numCourses, indegree);
         
     }
 };
