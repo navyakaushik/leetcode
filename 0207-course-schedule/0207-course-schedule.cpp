972d9bb08ab10fd96d4c0597cfb909fbafe44ac9
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-01 15:13:56 +0530

    Time: 6 ms (58.14%), Space: 20 MB (27.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,62 +1,53 @@
-#include <vector>
-#include <unordered_map>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        unordered_map<int, vector<int>> adj;  // adjacency list
-        vector<int> indegree(numCourses, 0);  // indegree array
-        
-        // Build adjacency list and indegree array
-        for (auto &vec : prerequisites) {
-            int a = vec[0];
-            int b = vec[1];
-            
-            // b --> a (b is prerequisite for a)
-            adj[b].push_back(a);
-            
-            // Increment indegree of course 'a'
-            indegree[a]++;
-        }
-        
-        // Check for cycle using topological sort
-        return topologicalSortCheck(adj, numCourses, indegree);
-    }
     
-private:
-    bool topologicalSortCheck(unordered_map<int, vector<int>> adj, int n, vector<int>& indegree) {
+    bool topologicalSort(unordered_map<int, vector<int>>& adj, int n, vector<int>& indegree){
         queue<int> que;
         int count = 0;
-        
-        // Enqueue all courses with indegree 0
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                count++;
                 que.push(i);
             }
         }
         
-        // Process the queue
-        while (!que.empty()) {
+        while(!que.empty()){
             int u = que.front();
             que.pop();
-            count++;  // Increment count when processing a node
             
-            // Decrease the indegree of neighbors
-            for (int &v : adj[u]) {
+            for(int &v : adj[u]){
                 indegree[v]--;
                 
-                // If indegree becomes 0, add to queue
-                if (indegree[v] == 0) {
+                if(indegree[v] == 0){
+                    count++;
                     que.push(v);
                 }
             }
         }
         
-        // If all courses are processed, no cycle exists
-        return count == n;
+        if(count == n) return true;
+        
+        return false;
+        
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        unordered_map<int, vector<int>> adj;
+        
+        vector<int> indegree(numCourses, 0);
+        
+        for(auto &vec : prerequisites){
+            int a = vec[0];
+            int b = vec[1];
+            
+            // b se a arrow 
+            adj[b].push_back(a);
+            
+            // arrow aaya to indegree increase 
+            indegree[a]++;
+        }
+        
+        return topologicalSort(adj, numCourses, indegree);
     }
 };
