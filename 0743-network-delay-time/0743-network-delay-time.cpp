e1c89517ed8822dd0585fb8bebb7a598b685e98b
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-02 12:00:02 +0530

    Time: 87 ms (45.17%), Space: 44.3 MB (57.26%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,46 +1,58 @@
 class Solution {
 public:
+    
+    
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+        greater<pair<int, int>>> pq;
         unordered_map<int, vector<pair<int, int>>> adj;
-
-        // Build adjacency list
+        
+        //build adjacency list
+        
         for(auto &vec : times){
             int u = vec[0];
             int v = vec[1];
             int w = vec[2];
-            adj[u].push_back({v, w});
+            adj[u].push_back({v,w});
         }
-
-        // Initialize distances
-        vector<int> result(n + 1, INT_MAX);
+        
+        // initialize distances 
+        
+        vector<int> result(n+1, INT_MAX);
         result[k] = 0;
         pq.push({0, k});
-
-        // Dijkstra's algorithm
+        
+        // dijiktra's algorithm 
+        
         while(!pq.empty()){
             int d = pq.top().first;
             int node = pq.top().second;
             pq.pop();
-
-            for(auto &vec : adj[node]){
-                int adjNode = vec.first;
-                int dist = vec.second;
-
-                if(d + dist < result[adjNode]){
-                    result[adjNode] = d + dist;
-                    pq.push({d + dist, adjNode});
-                }
+        
+        
+        for(auto &vec : adj[node]){
+            int adjNode = vec.first;
+            int dist = vec.second;
+            
+            if(d + dist < result[adjNode]){
+                result[adjNode] = d + dist;
+                pq.push({d+dist, adjNode});
             }
         }
-
-        // Calculate the maximum time taken
-        int ans = INT_MIN;
-        for(int i = 1; i <= n; i++){  // Start from 1 to ignore unused index 0
-            if(result[i] == INT_MAX) return -1; // If any node is unreachable
-            ans = max(ans, result[i]);
-        }
-
-        return ans;
+        
     }
+    
+    // calculate minimum time taken
+    
+    int ans = INT_MIN;
+    for(int i = 1; i <= n; i++){
+        if(result[i] == INT_MAX) return -1;
+        ans = max(ans, result[i]);
+    }
+    
+    return ans;
+        
+    }
+    
 };
