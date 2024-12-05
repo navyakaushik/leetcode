18f9535b19b161301f1077ae1254e1c7cd6c8c53
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-05 15:37:55 +0530

    Time: 45 ms (84.25%), Space: 98.3 MB (17.59%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    bool possibleHai(vector<int>& time, long long givenTime, int totalTrips){
+        long long actualTrips = 0;
+        for(int &t : time){
+            actualTrips += givenTime / t;
+        }
+        return actualTrips >= totalTrips;
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        
+        int n = time.size();
+        long long l = 1;
+        long long r = (long long) * min_element(begin(time), end(time)) * totalTrips;
+        
+        while(l < r){
+            long long mid_time = l+( r - l ) / 2;
+            if(possibleHai(time, mid_time, totalTrips)){
+                r = mid_time;
+            }else{
+                l  = mid_time + 1;
+            }
+        }
+        return l;
+        
+    }
+};
