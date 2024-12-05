0ac2141cb663545a523b63b0a49c76d7c749c56c
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-05 16:28:44 +0530

    Time: 9 ms (56.01%), Space: 22.9 MB (11.09%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,55 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    
-    bool canEatAll(vector<int>&piles, int givenHour, int h){
-        int actualHour =0;
-        
-        for(int &x : piles){
-            actualHour += x/givenHour;
-            
-            if(x % givenHour != 0) actualHour ++;
+    // This function checks if Koko can eat all the bananas within the given hours `h`
+    // when eating at a rate of `givenHour` bananas per hour.
+    bool canEatAll(vector<int>& piles, int givenHour, int h) {
+        int actualHour = 0; // Keeps track of the total hours taken to eat all bananas.
+
+        // Iterate through each pile of bananas.
+        for (int &x : piles) {
+            // Add the number of hours required to eat all bananas from the current pile.
+            actualHour += x / givenHour;
+
+            // If there are leftover bananas in the pile after eating `givenHour` bananas,
+            // Koko will need an extra hour to finish the rest.
+            if (x % givenHour != 0) 
+                actualHour++;
         }
+
+        // Return true if the total hours required is less than or equal to `h`.
         return actualHour <= h;
     }
-    
-    int minEatingSpeed(vector<int>& piles, int h){
-        int n = piles.size();
-        
+
+    // Main function to find the minimum eating speed `k` such that all bananas are eaten in `h` hours.
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size(); // Number of banana piles.
+
+        // The minimum possible eating speed is 1 banana per hour.
         int l = 1;
+
+        // The maximum possible eating speed is the size of the largest pile.
         int r = *max_element(begin(piles), end(piles));
-        
-        while(l < r){
-            int mid = l + (r-l)/2;
-            
-            if(canEatAll(piles, mid, h)){
+
+        // Perform binary search to find the optimal eating speed.
+        while (l < r) {
+            // Calculate the middle speed in the current range.
+            int mid = l + (r - l) / 2;
+
+            // Check if Koko can eat all bananas at the current speed.
+            if (canEatAll(piles, mid, h)) {
+                // If yes, try reducing the eating speed.
                 r = mid;
-            }else{
-                l = mid+1;
+            } else {
+                // Otherwise, increase the speed.
+                l = mid + 1;
             }
         }
+
+        // The left pointer `l` now holds the minimum eating speed.
         return l;
     }
 };
