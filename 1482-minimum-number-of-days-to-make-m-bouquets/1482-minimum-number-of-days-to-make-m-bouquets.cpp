2189a4a8ec0b52e55df30977d12a6d5edd37496c
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-05 20:34:01 +0530

    Time: 15 ms (96.51%), Space: 70.6 MB (16.30%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,48 +1,43 @@
-
 class Solution {
 public:
-    // Calculate the minimum number of days to make 'm' bouquets
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        // If it's impossible to make m bouquets with k flowers
-        if (1LL * m * k > n) return -1;  // Use 1LL to prevent overflow
 
-        // Find the minimum and maximum days from bloomDay
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
-        int high = *max_element(bloomDay.begin(), bloomDay.end());
+    bool canMakeBouq(vector<int>& bloomDay, int mid, int k, int m){
+        int bouqCount = 0;
+        int consecutive_count = 0;
 
-        // Binary search to find the minimum day
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (getBouquetCount(bloomDay, k, mid) >= m)
-                high = mid;  // We try for earlier days
-            else
-                low = mid + 1;  // We try for later days
-        }
-
-        return low;
-    }
-
-private:
-    // Returns the number of bouquets that can be made with k adjacent flowers on or before `waitingDays`
-    int getBouquetCount(const vector<int>& bloomDay, int k, int waitingDays) {
-        int bouquetCount = 0;
-        int adjacentFlowers = 0;
-
-        for (int day : bloomDay) {
-            if (day <= waitingDays) {
-                // This flower bloomed, count it
-                adjacentFlowers++;
-                if (adjacentFlowers == k) {
-                    // We have enough flowers to make a bouquet
-                    bouquetCount++;
-                    adjacentFlowers = 0; // Reset for the next bouquet
-                }
-            } else {
-                adjacentFlowers = 0; // Reset if the flower hasn't bloomed
+        for(int i = 0; i < bloomDay.size(); i++){
+            if(bloomDay[i] <= mid){
+                consecutive_count ++;
+            } else{
+                consecutive_count = 0;
+            }
+            if ( consecutive_count == k){
+                bouqCount++;
+                consecutive_count = 0;
             }
         }
+        return bouqCount >= m;
+    }
 
-        return bouquetCount;
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+
+        int n = bloomDay.size();
+        int start_day = 0;
+        int end_day = *max_element(begin(bloomDay), end(bloomDay));
+
+        int min_days = -1;
+
+        while(start_day <= end_day){
+            int mid = start_day + (end_day - start_day) / 2;
+            if(canMakeBouq(bloomDay, mid, k,m)){
+                min_days = mid;
+                end_day = mid - 1; 
+            } else {
+                start_day = mid + 1;
+            }
+        }
+        return min_days;
+        
     }
 };
