0ba2a122c28f10ee761a52cfa20ae4e34b00f5ea
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-10 23:30:03 +0530

    Time: 11 ms (88.64%), Space: 58 MB (78.77%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,55 +10,24 @@
  */
 class Solution {
 public:
-    ListNode* findMiddle(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-    
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy;
-        
-        // Merge lists
-        while (list1 != NULL && list2 != NULL) {
-            if (list1->val < list2->val) {
-                temp->next = list1;
-                temp = list1;
-                list1 = list1->next;
-            } else {
-                temp->next = list2;
-                temp = list2;
-                list2 = list2->next;
-            }
-        }
-        
-        // Attach the remaining part of the list
-        if (list1) temp->next = list1;
-        else temp->next = list2;
-        
-        return dummy->next;
-    }
-    
     ListNode* sortList(ListNode* head) {
-        // Base case: If the list is empty or has only one element
-        if (head == NULL || head->next == NULL) return head;
+        vector<int> arr;
+        ListNode* temp = head;
         
-        // Find the middle of the list
-        ListNode* middle = findMiddle(head);
-        ListNode* right = middle->next;
-        middle->next = nullptr;
-        ListNode* left = head;
+        while(temp != NULL){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
         
-        // Sort the left and right halves recursively
-        left = sortList(left);
-        right = sortList(right);
+        sort(arr.begin(), arr.end());
         
-        // Merge the sorted halves
-        return mergeTwoLists(left, right);
+        temp = head;
+        
+        for(int i = 0; i < arr.size(); i++){
+            temp->val = arr[i];
+            temp = temp->next;
+        }
+        return head;
+        
     }
 };
