9d51bbd9b9436ff609a52b1703c2abe87c73fb26
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-10 16:20:41 +0530

    Time: 4 ms (95.62%), Space: 11.7 MB (27.81%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,7 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     bool hasCycle(ListNode *head) {
         
@@ -28,5 +28,30 @@ public:
         }
         return NULL;
         
+    }
+};
+*/
+
+class Solution{
+    public: 
+    bool hasCycle(ListNode* head){
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
     }
 };
