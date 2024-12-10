a22d145a91d7fc6ec2e30db49d24a06e60f1edba
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-11 00:00:59 +0530

    Time: 58 ms (12.35%), Space: 75.8 MB (6.99%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,24 +10,56 @@
  */
 class Solution {
 public:
+    
+    ListNode* mergeSort(ListNode* list1, ListNode* list2){
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
+                temp->next = list1;
+                list1 = list1->next;
+            }else{
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+        if(list1 != NULL){
+            temp->next = list1;
+        }else{
+            temp->next = list2;
+        }
+        return dummyNode->next;
+    }
+    
+    ListNode* middleElement(ListNode* head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
     ListNode* sortList(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
-        
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        if(head == NULL || head->next == NULL){
+            return head;
         }
+        ListNode* middle = middleElement(head);
         
-        sort(arr.begin(), arr.end());
+        ListNode* right = middle->next;
+        middle->next = NULL;
+        ListNode* left = head;
         
-        temp = head;
+        left = sortList(left);
+        right = sortList(right);
         
-        for(int i = 0; i < arr.size(); i++){
-            temp->val = arr[i];
-            temp = temp->next;
-        }
-        return head;
+        return mergeSort(left, right);
         
     }
 };
