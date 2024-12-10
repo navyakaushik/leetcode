a6ebce987c4e90f393cd430afbb682d79c475c04
Navya Kaushik <124089440+navyakaushik@users.noreply.github.com>
2024-12-10 15:48:26 +0530

    Time: 0 ms (100.00%), Space: 10 MB (10.29%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,18 +8,16 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        
+class Solution{
+    public: 
+    ListNode* middleNode(ListNode* head){
         ListNode* slow = head;
         ListNode* fast = head;
-        
-        while(fast != NULL && fast -> next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
+            
+            while(fast != NULL && fast->next != NULL){
+                slow = slow->next;
+                fast = fast->next->next;
+            }
         return slow;
-        
     }
 };
